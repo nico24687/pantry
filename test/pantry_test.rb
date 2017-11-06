5029b7c0cea6329180cc2e61ac919a038cc580be
nico24687 <theonlynico@hotmail.com>
2017-11-06 13:07:14 -0700

    able to check stock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,22 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_has_empty_stock_by_default
+    pantry = Pantry.new
+
+    assert_equal({}, pantry.stock)
+  end
+
+  def test_it_can_check_stock
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
 end
